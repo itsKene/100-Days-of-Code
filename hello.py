454adcfd8e39c40bf733df1589186268fa58dd59
Gabriel Okenwa <gabrielokenwa@gmail.com>
2023-04-10 15:52:03 -0100

    Init

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+    name = input("What's your name?")
+    check = input("Do you accept the '100 days of code' challenge? (y/n)").lower()
+
+    if check == 'y':
+        print(f"Hello{name}, Thanks for accepting the challenge")
+    elif check == 'n':
+        print(f"Hello{name}, Oops, the challenge would have been fun")
+    else:
+        print("invalid input, cheers!")
+    
