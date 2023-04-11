1efb6cf512d6e703079aba84e76838b1b99aa24b
Gabriel Okenwa <gabrielokenwa@gmail.com>
2023-04-11 02:41:57 -0100

    bug fixes

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 def main():
-    name = input("What's your name?")
-    check = input("Do you accept the '100 days of code' challenge? (y/n)").lower()
+    name = input("What's your name? ")
+    check = input("Do you accept the '100 days of code' challenge? (y/n) ").lower()
 
     if check == 'y':
-        print(f"Hello{name}, Thanks for accepting the challenge")
+        print(f"Hello {name}, Thanks for accepting this challenge")
     elif check == 'n':
-        print(f"Hello{name}, Oops, the challenge would have been fun")
+        print(f"Hello {name}, Oops, the challenge would have been fun")
     else:
         print("invalid input, cheers!")
 
